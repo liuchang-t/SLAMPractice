a3fa082a12b18763b3389f4c0c0b478b8f74e0ec
Chang Liu <liuchang_t@163.com>
2020-03-31 09:21:41 +0800

    基本VO，PnP实现，无优化

---
+++ src/main.cpp
@@ -0,0 +1,130 @@
+// -------------- test the visual odometry -------------
+#include <fstream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/viz.hpp> 
+//#include <boost/timer/timer.hpp>
+#include "myslam/config.h"
+#include "myslam/visual_odometry.h"
+
+int main(int argc, char** argv)
+{
+    cout << "argc = " << argc << endl;
+    if (argc == 1)
+    {
+        // 没有输入参数，那就用内置的参数
+        myslam::Config::setParameterFile("C:/Users/LiuChang/source/repos/SLAMPractice/config/default.yaml");
+    }
+    else if (argc == 2)
+    {
+        // 输入参数为参数文件完整路径包括名称
+        myslam::Config::setParameterFile(argv[1]);
+    }
+    else
+    {
+        cout << "usage: run_vo parameter_file" << endl;
+        return 1;
+    }
+    // myslam::Config::get<double>("scale_factor");
+    myslam::Camera::Ptr camera(new myslam::Camera(
+        myslam::Config::get<float>("camera.fx"),
+        myslam::Config::get<float>("camera.fy"),
+        myslam::Config::get<float>("camera.cx"),
+        myslam::Config::get<float>("camera.cy"),
+        myslam::Config::get<float>("camera.depth_scale")));
+
+    // config文件中保存的有将要使用的数据集的存储路径，程序运行前记得检查config文件内容是否正确
+    string dataset_dir = myslam::Config::get<string>("dataset_dir");
+    cout << "dataset: " << dataset_dir << endl;
+    // associate.txt文件中是彩色图像和深度图像的时间戳对齐信息，这个文件由associate.py脚本，
+    // 通过读取数据集中的rgb.txt和depth.txt两个文件之后，对其中内容进行匹配然后生成
+    ifstream fin(dataset_dir + "/associate.txt");
+    if (!fin)
+    {
+        cout << "please generate the associate file called associate.txt!" << endl;
+        return 1;
+    }
+    // 从associate.txt文件中一行一行地读出彩色和深度图像的时间戳的文件名称，分别保存在不同的数组里
+    vector<string> rgb_files, depth_files;
+    vector<double> rgb_times, depth_times;
+    while (!fin.eof())
+    {
+        string rgb_time, rgb_file, depth_time, depth_file;
+        fin >> rgb_time >> rgb_file >> depth_time >> depth_file;
+        rgb_times.push_back(atof(rgb_time.c_str()));
+        depth_times.push_back(atof(depth_time.c_str()));
+        rgb_files.push_back(dataset_dir + "/" + rgb_file);
+        depth_files.push_back(dataset_dir + "/" + depth_file);
+
+        if (fin.good() == false)
+            break;
+    }
+
+    // visualization
+    // 使用viz模块创建可视化窗口，并初始化世界坐标系、相机坐标系、初始视角等等
+    cv::viz::Viz3d vis("Visual Odometry");
+    cv::viz::WCoordinateSystem world_coor(1.0), camera_coor(0.5);
+    cv::Point3d cam_pos(0, -1.0, -1.0), cam_focal_point(0, 0, 0), cam_y_dir(0, 1, 0);
+    cv::Affine3d cam_pose = cv::viz::makeCameraPose(cam_pos, cam_focal_point, cam_y_dir);
+    vis.setViewerPose(cam_pose);
+
+    world_coor.setRenderingProperty(cv::viz::LINE_WIDTH, 2.0);
+    camera_coor.setRenderingProperty(cv::viz::LINE_WIDTH, 1.0);
+    vis.showWidget("World", world_coor);
+    vis.showWidget("Camera", camera_coor);
+
+    // 准备依次处理图片
+    cout << "read total " << rgb_files.size() << " entries" << endl;
+    myslam::VisualOdometry::Ptr vo(new myslam::VisualOdometry);
+    for (int i = 0; i < rgb_files.size(); i++)
+    {
+        // imread 读取图片
+        Mat color = cv::imread(rgb_files[i]);
+        Mat depth = cv::imread(depth_files[i], -1);
+        if (color.data == nullptr || depth.data == nullptr)
+            break;
+        // 将读取到的图像数据和时间戳整合为一个 Frame 帧对象
+        myslam::Frame::Ptr pFrame = myslam::Frame::createFrame();
+        pFrame->camera_ = camera;
+        pFrame->color_ = color;
+        pFrame->depth_ = depth;
+        pFrame->time_stamp_ = rgb_times[i];
+
+        // boost::timer timer_1;
+        // 向 VisualOdometry 对象中加入该 Frame 帧
+        // addFrame()是VisualOdometry的主要函数，其包含了特征提取、描述子计算、图像匹配、位姿估计、参考帧数据更新等一系列流程
+        vo->addFrame(pFrame);
+        // cout<<"VO costs time: "<<timer_1.elapsed()<<endl;
+
+        // 做一次检验，如果vo处于跟丢的状态，就退出循环
+        if (vo->state_ == myslam::VisualOdometry::LOST)
+            break;
+        // 如果vo还没有跟丢，那就获取当前相机在世界坐标系中的位姿，在addFrame()的时候，会更新当前帧的Tcw矩阵，Tcw矩阵代表了
+        // 从世界坐标系到相机坐标系的位姿变换矩阵，他由旋转矩阵和平移向量组成，其中旋转矩阵代表了世界坐标系在相机坐标系下的姿态，
+        // 而平移向量则代表了世界坐标系的原点在相机坐标系中的坐标。
+        // 因此，对Tcw求逆矩阵，就可以得到相机坐标系在世界坐标系下的位姿。
+        SE3d Twc = pFrame->T_c_w_.inverse();
+
+        // show the map and the camera pose 
+        // 需要使用cv::Affine3类来进行viz模块坐标系位置的更新，cv::Affine3对象的核心其实就是一个4*4的位姿变换矩阵，根据其定义，
+        // 可以使用旋转矩阵R和平移向量t作为参数来声明这个对象，只不过它需要的矩阵和向量如下：
+        // 由于SE3d
+        cv::Affine3d M(
+            cv::Affine3d::Mat3(
+                Twc.rotationMatrix()(0, 0), Twc.rotationMatrix()(0, 1), Twc.rotationMatrix()(0, 2),
+                Twc.rotationMatrix()(1, 0), Twc.rotationMatrix()(1, 1), Twc.rotationMatrix()(1, 2),
+                Twc.rotationMatrix()(2, 0), Twc.rotationMatrix()(2, 1), Twc.rotationMatrix()(2, 2)
+                ),
+            cv::Affine3d::Vec3(
+                Twc.translation()(0, 0), Twc.translation()(1, 0), Twc.translation()(2, 0)
+                )
+            );
+
+        cv::imshow("image", color );
+        cv::waitKey(1); 
+        vis.setWidgetPose("Camera", M);
+        vis.spinOnce(1, false);
+    }
+
+    return 0;
+}
---
+++ src/visual_odometry.cpp
@@ -0,0 +1,233 @@
+﻿#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <algorithm>
+
+#include "myslam/config.h"
+#include "myslam/visual_odometry.h"
+
+namespace myslam
+{
+    VisualOdometry::VisualOdometry() :
+        state_(INITIALIZING), ref_(nullptr), curr_(nullptr), map_(new Map), num_lost_(0), num_inliers_(0)
+    {
+        num_of_features_ = Config::get<int>("number_of_features");
+        scale_factor_ = Config::get<double>("scale_factor");
+        level_pyramid_ = Config::get<int>("level_pyramid");
+        match_ratio_ = Config::get<float>("match_ratio");
+        max_num_lost_ = Config::get<float>("max_num_lost");
+        min_inliers_ = Config::get<int>("min_inliers");
+        key_frame_min_rot = Config::get<double>("keyframe_rotation");
+        key_frame_min_trans = Config::get<double>("keyframe_translation");
+        orb_ = cv::ORB::create(num_of_features_, scale_factor_, level_pyramid_);
+    }
+
+    VisualOdometry::~VisualOdometry()
+    {
+
+    }
+
+    bool VisualOdometry::addFrame(Frame::Ptr frame)
+    {
+        switch (state_)
+        {
+        case INITIALIZING:
+        {
+            state_ = OK;
+            curr_ = ref_ = frame;
+            map_->insertKeyFrame(frame);
+            // extract features from first frame 
+            extractKeyPoints();
+            computeDescriptors();
+            // compute the 3d position of features in ref frame 
+            setRef3DPoints();
+            break;
+        }
+        case OK:
+        {
+            curr_ = frame;
+            extractKeyPoints();
+            computeDescriptors();
+            featureMatching();
+            poseEstimationPnP();
+            if (checkEstimatedPose() == true) // a good estimation
+            {
+                // 更新当前帧的Tcw矩阵，T_c_w = T_c_r*T_r_w 
+                curr_->T_c_w_ = T_c_r_estimated_ * ref_->T_c_w_;  // T_c_w = T_c_r*T_r_w 
+                ref_ = curr_;
+                setRef3DPoints();
+                num_lost_ = 0;
+                if (checkKeyFrame() == true) // is a key-frame
+                {
+                    addKeyFrame();
+                }
+            }
+            else // bad estimation due to various reasons
+            {
+                num_lost_++;
+                if (num_lost_ > max_num_lost_)
+                {
+                    state_ = LOST;
+                }
+                return false;
+            }
+            break;
+        }
+        case LOST:
+        {
+            cout << "vo has lost." << endl;
+            break;
+        }
+        }
+
+        return true;
+    }
+
+    // 根据当前帧的 color_ 图像信息，提取出所有的特征点，存储到 keypoints_curr_ 数组中
+    void VisualOdometry::extractKeyPoints()
+    {
+        orb_->detect(cv::InputArray(curr_->color_), keypoints_curr_);
+    }
+
+    // 根据提取出的特征点，计算当前帧所有特征点的描述子信息，存储到 descriptors_curr_ 数组中
+    void VisualOdometry::computeDescriptors()
+    {
+        orb_->compute(curr_->color_, keypoints_curr_, descriptors_curr_);
+    }
+
+    // 根据当前帧和参考帧的特征点描述子信息，进行特征匹配，将匹配结果存储到 feature_matches_ 数组中
+    void VisualOdometry::featureMatching()
+    {
+        // match desp_ref and desp_curr, use OpenCV's brute force match 
+        vector<cv::DMatch> matches;
+        cv::BFMatcher matcher(cv::NORM_HAMMING);
+        matcher.match(descriptors_ref_, descriptors_curr_, matches);
+        // select the best matches
+        float min_dis = std::min_element(
+            matches.begin(), matches.end(),
+            [](const cv::DMatch& m1, const cv::DMatch& m2)
+            {
+                return m1.distance < m2.distance;
+            })->distance;
+
+        feature_matches_.clear();
+        for (cv::DMatch& m : matches)
+        {
+            if (m.distance < max<float>(min_dis * match_ratio_, 30.0))
+            {
+                feature_matches_.push_back(m);
+            }
+        }
+        // cout<<"good matches: "<<feature_matches_.size()<<endl;
+    }
+
+    // setRef3DPoints()函数将当前帧的有效关键点的3D坐标和描述子信息依次存储到 pts_3d_ref_ 和 descriptors_ref_ 数组中
+    // 这个3D坐标是在对应帧的相机坐标系下的表示
+    void VisualOdometry::setRef3DPoints()
+    {
+        // select the features with depth measurements 
+        pts_3d_ref_.clear();
+        descriptors_ref_ = Mat();
+        // 对每一个关键点进行操作
+        for (size_t i = 0; i < keypoints_curr_.size(); i++)
+        {
+            // 获取图像上这个点的深度信息
+            double d = ref_->findDepth(keypoints_curr_[i]);
+            // 只有深度信息大于0的才算有效
+            if (d > 0)
+            {
+                // 获得这个点在相机坐标系下的完整3d坐标
+                Vector3d p_cam = ref_->camera_->pixel2camera(
+                    Vector2d(keypoints_curr_[i].pt.x, keypoints_curr_[i].pt.y), d
+                    );
+                // 把这个特征点的3d坐标加入 pts_3d_ref_ 数组
+                pts_3d_ref_.push_back(cv::Point3f(p_cam(0, 0), p_cam(1, 0), p_cam(2, 0)));
+                // 把这个点的描述子加入 descriptors_ref_ 数组，描述子对应的是 descriptors_curr_ 的第i行
+                descriptors_ref_.push_back(descriptors_curr_.row(i));
+            }
+        }
+    }
+    // 根据特征匹配的结果 feature_matches_，参考帧关键点位置pts_3d_ref_，
+    // 当前帧关键点坐标keypoints_curr_，以及相机内参，估计参考帧到当前帧的相机位姿变换矩阵 T_c_r_estimated_
+    void VisualOdometry::poseEstimationPnP()
+    {
+        // construct the 3d 2d observations
+        vector<cv::Point3f> pts3d;
+        vector<cv::Point2f> pts2d;
+        // 根据特征匹配的结果索引，统计所有的匹配到的特征点在参考帧相机坐标系中的3D坐标以及在当前帧中的图像坐标
+        for (cv::DMatch m : feature_matches_)
+        {
+            pts3d.push_back(pts_3d_ref_[m.queryIdx]);
+            pts2d.push_back(keypoints_curr_[m.trainIdx].pt);
+        }
+        // 相机内参矩阵
+        Mat K = (cv::Mat_<double>(3, 3) <<
+            ref_->camera_->fx_, 0, ref_->camera_->cx_,
+            0, ref_->camera_->fy_, ref_->camera_->cy_,
+            0, 0, 1
+            );
+        // rvec是旋转向量，tvec是平移向量，inliers是通过RANSAC求解拟合出的有效数据点（内点），即拟合误差小于设定阈值（4.0）的点
+        Mat inliers;
+        cv::Mat distCoeffs = cv::Mat::zeros(4, 1, CV_64FC1);
+        cv::Mat rvec = cv::Mat::zeros(3, 1, CV_64FC1);          // output rotation vector
+        cv::Mat tvec = cv::Mat::zeros(3, 1, CV_64FC1);    // output translation vector
+        cv::solvePnPRansac(pts3d, pts2d, K, distCoeffs, rvec, tvec, false, 100, 4.0, 0.99, inliers);
+        num_inliers_ = inliers.rows;
+        // 打印输出PnP求解的内点数量
+        // cout<<"pnp inliers: "<<num_inliers_<<endl;
+
+        // 将Vector3d类型表示的旋转向量转换为 Eigen::Quaterniond ，然后才能用来构造 SO3 
+        // 步骤：先把 Vector3d 类型的旋转向量的模和方向提取出来构造 AngleAxis 类型的旋转向量，
+        // 然后再从 AngleAxis 转到 Quaterniond 
+        Eigen::Vector3d v_r(rvec.at<double>(0, 0), rvec.at<double>(1, 0), rvec.at<double>(2, 0));
+        Eigen::Vector3d v_t(tvec.at<double>(0, 0), tvec.at<double>(1, 0), tvec.at<double>(2, 0));
+        Eigen::AngleAxisd rotation_vector(v_r.norm(), v_r / v_r.norm());
+        Eigen::Quaterniond quater = Eigen::Quaterniond(rotation_vector);
+        ////////////////////////////////////////////////
+        // 其实有了四元数或者旋转矩阵之后，就可以和平移向量一起直接构造SE3
+        //cout << "v_t = " << v_t.transpose() << "        v_r = " << v_r.transpose() << endl;
+        //cout << "v_r.norm = " << v_r.norm() << endl;
+        T_c_r_estimated_ = SE3d(quater, v_t);
+
+    }
+
+    // 检查位姿估计是否正确，判断策略是内点数量是否足够，以及运动是否过大
+    bool VisualOdometry::checkEstimatedPose()
+    {
+        // check if the estimated pose is good
+        if (num_inliers_ < min_inliers_)
+        {
+            cout << "reject because inlier is too small: " << num_inliers_ << endl;
+            return false;
+        }
+        // if the motion is too large, it is probably wrong
+        Sophus::Vector6d d = T_c_r_estimated_.log();
+        if (d.norm() > 5.0)
+        {
+            cout << "reject because motion is too large: " << d.norm() << endl;
+            return false;
+        }
+        return true;
+    }
+
+    // 检查当前帧是否是关键帧，判断策略是检查平移向量和旋转向量的模是否大于阈值，
+    // 只有运动足够大才算关键帧
+    bool VisualOdometry::checkKeyFrame()
+    {
+        // 通过.log()函数把位姿变换矩阵转化为旋量表示
+        Sophus::Vector6d d = T_c_r_estimated_.log();
+        // 旋量的前三个元素是平移向量，后三个元素是旋转向量
+        Vector3d trans = d.head<3>();
+        Vector3d rot = d.tail<3>();
+        if (rot.norm() > key_frame_min_rot || trans.norm() > key_frame_min_trans)
+            return true;
+        return false;
+    }
+
+    // 将当前帧作为关键帧插入到 map_ 成员中
+    void VisualOdometry::addKeyFrame()
+    {
+        cout << "adding a key-frame" << endl;
+        map_->insertKeyFrame(curr_);
+    }
+}
